4b6546b48e18b4bd27ade89617b6679f6aa5fb75
Vlad Zaharia <vladzaharia@gmail.com>
2014-10-29 21:18:13 -0700

    Fix up urls

--- tcfv2/urls.py
+++ tcfv2/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
